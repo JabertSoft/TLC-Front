43c205dbabe158e188e3b36aa81deef6535ea127
robertoyanes <roberto.yanes@despegar.com>
2018-05-22 17:07:40 -0300

    home-sale

--- TheLuxuryChoice/src/app/app.routing.ts
+++ TheLuxuryChoice/src/app/app.routing.ts
@@ -6,12 +6,14 @@ import {HomeComponent} from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { AboutComponent } from './about/about.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeSaleComponent } from './home-sale/home-sale.component';
 
 const appRoutes: Routes = [
         {path:'', component:HomeComponent},
         {path:'home', component:HomeComponent},
         {path:'about', component:AboutComponent},
         {path:'signup', component:SignUpComponent},
+        {path:'sale', component:HomeSaleComponent},
         {path:'**', component:ErrorComponent},
 ];
 

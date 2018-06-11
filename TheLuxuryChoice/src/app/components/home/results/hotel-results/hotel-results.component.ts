4e9e0d85dcaeac3a6f6bfe840c67c4dcf6b5b045
Javier Lopez Malone <malonejv@gmail.com>
2018-06-11 13:38:35 -0300

    puto el que lee

--- TheLuxuryChoice/src/app/components/home/results/hotel-results/hotel-results.component.ts
+++ TheLuxuryChoice/src/app/components/home/results/hotel-results/hotel-results.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, forwardRef  } from '@angular/core';
+import { AppComponent } from '../../../../app.component';
+declare var $:any;
+declare var jQuery:any;
 @Component({
   selector: 'app-hotel-results',
   templateUrl: './hotel-results.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelResultsComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(forwardRef(() => AppComponent)) private main:AppComponent) { }
 
   ngOnInit() {
   }
 
+  changeLanguage(language?:string){
+    this.main.changeLanguage(language);
+  }
+  
+  menu_click(){
+    $(".menu-button").toggleClass("change");
+    $('.main-nav, .menu-btn').toggleClass('active');
+    
+}
 }

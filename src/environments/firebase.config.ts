474e702488e30e6aa6962a8bd7b9e40eba1466af
Roberto <roberto@iMac-de-Roberto.local>
2018-07-19 00:48:19 -0300

    -pwa

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAsZZS_c_g524_rpoutpDiQ46UtK4SobGE",
+    authDomain: "tlcpwa.firebaseapp.com",
+    databaseURL: "https://tlcpwa.firebaseio.com",
+    projectId: "tlcpwa",
+    storageBucket: "tlcpwa.appspot.com",
+    messagingSenderId: "238585086688"
+  };
